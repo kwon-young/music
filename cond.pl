81c551b2f727df3afaa6cbcf3cc30af4b1050855
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2023-04-29 08:46:17 +0200

    remove useless conditions

--- cond.pl
+++ cond.pl
@@ -1,5 +1,4 @@
-:- module(cond, [ccxEtiqsCond/2, ccxEtiqsCond/3, ccxOnSegCond/3,
-                 horizontalCond/1, verticalCond/1]).
+:- module(cond, [ccxEtiqsCond/2, ccxEtiqsCond/3, ccxOnSegCond/3]).
 
 :- use_module(library(clpBNR)).
 :- use_module(ccx).
@@ -16,14 +15,4 @@ ccxEtiqsCond(Ccx, N, Etiq) :-
 ccxOnSegCond(Seg, Ccx, Eps) :-
   ccxOrigin(Ccx, point(OriginX, OriginY)),
   segYAtX(Seg, SegY, OriginX),
-  diffEps(Eps, OriginY, SegY).
-
-horizontalCond(Seg) :-
-  segStartEnd(Seg, point(X1, Y1), point(X2, Y2)),
-  [X1, Y1, X2, Y2]::real,
-  {abs(X2 - X1) > abs(Y2 - Y1)}.
-
-verticalCond(Seg) :-
-  segStartEnd(Seg, point(X1, Y1), point(X2, Y2)),
-  [X1, Y1, X2, Y2]::real,
-  {abs(X2 - X1) < abs(Y2 - Y1)}.
+  eps(Eps, OriginY, SegY).
