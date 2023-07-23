c200a947c7ddf1d074f11d8f9e1ff1cbf6700ef4
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2023-07-23 14:32:09 +0200

    use module(library(ninja))

--- ninja.pl
+++ ninja.pl
@@ -1,41 +1,6 @@
 :- use_module(library(dcg/basics)).
 :- use_module(library(dcg/high_order)).
-
-identity(X) -->
-  X.
-
-indent([]) --> [].
-indent([H | T]) -->
-  sequence("  ", identity, "", "", [H | T]).
-
-ws(List) -->
-  sequence(identity, " ", List).
-ws(Start, List) -->
-  ws_(List, Start).
-ws_([], _) -->
-  [].
-ws_([H | T], Start) -->
-  sequence(Start, identity, " ", "", [H | T]).
-
-variable(Name-Value) -->
-  variable(Name, Value).
-variable(Name, Value) -->
-  ws([Name, "=", Value]), "\n".
-
-rule(Name, Command) -->
-  rule(Name, Command, []).
-rule(Name, Command, Variables) -->
-  "rule ", Name, "\n",
-  indent([variable("command", Command) | Variables]).
-
-build([H | T], Rule, Ins) -->
-  build([H | T], Rule, Ins, [], []).
-build([H | T], Rule, Ins, ImplicitIns) -->
-  build([H | T], Rule, Ins, ImplicitIns, []).
-build([H | T], Rule, Ins, ImplicitIns, Variables) -->
-  "build ", ws([H | T]), ": ", Rule, " ", ws(Ins), " ",
-  ws("| ", ImplicitIns), "\n",
-  indent(Variables).
+:- use_module(library(ninja)).
 
 verovio -->
   "/home/kwon-young/prog/verovio/build/verovio -r /home/kwon-young/prog/verovio/data ".
@@ -45,11 +10,19 @@ swipl -->
 stem_page(_, 1).
 
 stem("stafflines").
-% stem("double-stafflines").
+stem("double-stafflines").
+stem("brace").
+stem("bracket").
+stem("brace-bracket").
 stem("staff").
 stem("clef-G-2").
 stem("clef-F-4").
 stem("clef-staff").
+stem("meter-3-8").
+stem("dynamicpp").
+stem("quarter").
+stem("accid-sharp").
+stem("rest-8th").
 
 dir -->
    "data".
@@ -93,38 +66,36 @@ setting(Stem, Target) -->
 
 deps -->
   deps("music.pl").
-deps(Source) -->
-  Source, " ",
-  { xref_source(Source) },
-  foreach(xref_uses_file(Source, _, Path), atom(Path), " ").
 
 graph -->
-  rule("generate", (swipl, "-s $in -g main"), [variable("generator", "1")]),
-  rule("add_ids", (verovio, "--xml-id-checksum -t mei -o $out $in")),
-  rule("mei2svg", (verovio, "-a --footer none -o $output $in")),
-  rule("svg2pl", "python $in $out"),
-  rule("remove_scopes", (swipl, "-s music.pl -g main -- remove_scopes $in $out")),
-  rule("test", (swipl, "-s music.pl -g main -- mainTest $in $out")),
-  rule("reco", (swipl, "-s music.pl -g main -- mainReco $in $out")),
-  rule("gen", (swipl, "-s music.pl -g main -- mainGen $in $out")),
+  rule(generate, (swipl, "-s $in -g main"), [generator-"1"]),
+  rule(add_ids, (verovio, "--xml-id-checksum -t mei -o $out $in")),
+  rule(mei2svg, (verovio, "--use-brace-glyph -a --footer none -o $output $in")),
+  rule(svg2pl, "python $in $out"),
+  rule(remove_scopes, (swipl, "-s music.pl -g main -- remove_scopes $in $out")),
+  rule(test, (swipl, "-s music.pl -g main -- mainTest $in $out")),
+  rule(reco, (swipl, "-s music.pl -g main -- mainReco $in $out")),
+  rule(gen, (swipl, "-s music.pl -g main -- mainGen $in $out")),
 
-  build(["build.ninja"], "generate", ["ninja.pl"]),
+  build(["build.ninja"], generate, ["ninja.pl"], [implicit_ins([deps("ninja.pl")])]),
   foreach(stem(Stem), graph(Stem)),
-  build(["all"], "phony", [
+  build(["all"], phony, [
     foreach(stem(Stem), pl(Stem, "music"), " "),
     foreach(stem(Stem), mei(Stem, "music"), " "),
     foreach(stem(Stem), setting(Stem, "reco"), " ")
   ]).
 
 graph(Stem) -->
-  build([id(Stem)], "add_ids", [input(Stem)]),
-  build([svgs(Stem)], "mei2svg", [id(Stem)], [], [variable("output", svg(Stem))]),
-  build([pl(Stem)], "svg2pl", ["svg2pl.py", svgs(Stem), "data/glyphnames.json"]),
-  build([pl_noscope(Stem)], "remove_scopes", [pl(Stem)], [deps("geo.pl")]),
-  build([setting(Stem, "test")], "test", [id(Stem), pl(Stem)], [deps]),
-  build([mei(Stem, "music"), setting(Stem, "reco")], "reco",
-        [pl_noscope(Stem), setting(Stem, "test")], [deps]),
-  build([pl(Stem, "music")], "gen", [id(Stem)], [deps]).
+  build([id(Stem)], add_ids, [input(Stem)]),
+  build([svgs(Stem)], mei2svg, [id(Stem)], [variables([output-svg(Stem)])]),
+  build([pl(Stem)], svg2pl, ["svg2pl.py", svgs(Stem), "data/glyphnames.json"]),
+  build([pl_noscope(Stem)], remove_scopes, [pl(Stem)],
+        [implicit_ins([deps("geo.pl")])]),
+  build([setting(Stem, "test")], test, [id(Stem), pl(Stem)],
+        [implicit_ins([deps])]),
+  build([mei(Stem, "music"), setting(Stem, "reco")], reco,
+        [pl_noscope(Stem), setting(Stem, "test")], [implicit_ins([deps])]),
+  build([pl(Stem, "music")], gen, [id(Stem)], [implicit_ins([deps])]).
 
 main :-
   phrase(graph, L),
