d1529a5e06bb19af0b753e5795dc9ed4f39337f8
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2026-01-31 14:41:33 +0100

    refactor: Rename mei_transform to transform and remove redundant clause

--- mei.pl
+++ mei.pl
@@ -11,40 +11,28 @@ vu(Atom, Number) :-
   delay(atom_number(AtomNumber, Number)),
   delay(atom_concat(AtomNumber, 'vu', Atom)).
 
-mei_transform(meiversion=Atom, meiversion=N) :-
+transform(meiversion=Atom, meiversion=N) :-
   delay(atom_number(Atom, N)).
-
-mei_transform(n=Atom, n=N) :-
+transform(n=Atom, n=N) :-
   delay(atom_number(Atom, N)).
-
-mei_transform(lines=Atom, lines=N) :-
+transform(lines=Atom, lines=N) :-
   delay(atom_number(Atom, N)).
-
-mei_transform(line=Atom, line=N) :-
+transform(line=Atom, line=N) :-
   delay(atom_number(Atom, N)).
-
-mei_transform(count=Atom, count=N) :-
+transform(count=Atom, count=N) :-
   delay(atom_number(Atom, N)).
-
-mei_transform(unit=Atom, unit=N) :-
+transform(unit=Atom, unit=N) :-
   delay(atom_number(Atom, N)).
-
-mei_transform(ho=Atom, ho=N) :-
+transform(ho=Atom, ho=N) :-
   delay(atom_number(Atom, N)).
-
-mei_transform(staff=StaffAtom, staff=StaffList) :-
+transform(staff=StaffAtom, staff=StaffList) :-
   delay((
       atomic_list_concat(Atoms, ' ', StaffAtom),
       maplist(atom_number, Atoms, StaffList)
   )).
-
-mei_transform(dur=Atom, dur=N) :-
+transform(dur=Atom, dur=N) :-
   delay(atom_number(Atom, N)).
-
-mei_transform(oct=Atom, oct=N) :-
+transform(oct=Atom, oct=N) :-
   delay(atom_number(Atom, N)).
-
-mei_transform(len=Atom, len=N) :-
+transform(len=Atom, len=N) :-
   delay(vu(Atom, N)).
-
-mei_transform(Term, Term).
