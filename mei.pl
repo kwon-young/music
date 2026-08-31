71baee195f6db6472a635366d9be850abc2706b1
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2026-08-31 15:44:08 +0200

    snapshot mei

--- mei.pl
+++ mei.pl
@@ -1,8 +1,25 @@
-:- module(mei, [mei_transform/2]).
+:- module(mei, []).
 
 :- use_module(library(delay)).
 :- use_module(library(apply)).
 :- use_module(library(lists)).
+
+load_mei(Filename, Mei) :-
+  ( file_name_extension(_, mei, Filename)
+  -> load_xml(Filename, Xml, [space(remove), number(integer)]),
+    mapsubterms(parse, Xml, Mei)
+  ; file_name_extension(_, pl, Filename),
+    open(Filename, read, S),
+    read(S, Mei),
+    close(S)
+  ).
+
+mei_write(Filename, Mei) :-
+  open(XmlFile, write, XmlS),
+  ( ground(Xml)
+  -> xml_write(XmlS, Xml, [])
+  ; print_term(Xml, [output(XmlS)])
+  ),
 
 :- multifile delay:mode/1.
 
@@ -12,46 +29,42 @@ vu(Atom, Number) :-
   delay(atom_number(AtomNumber, Number)),
   delay(atom_concat(AtomNumber, 'vu', Atom)).
 
-mei_transform(meiversion=Atom, meiversion=N) :-
+gen(A, B) :- parse(B, A).
+
+parse(meiversion=Atom, meiversion=N) :-
   delay(atom_number(Atom, N)).
-mei_transform(lines=Atom, lines=N) :-
+parse(lines=Atom, lines=N) :-
   delay(atom_number(Atom, N)).
-mei_transform(line=Atom, line=N) :-
+parse(line=Atom, line=N) :-
   delay(atom_number(Atom, N)).
-mei_transform(count=Atom, count=N) :-
+parse(count=Atom, count=N) :-
   delay(atom_number(Atom, N)).
-mei_transform(ho=Atom, ho=N) :-
+parse(ho=Atom, ho=N) :-
   delay(atom_number(Atom, N)).
-mei_transform(staff=StaffAtom, staff=StaffList) :-
+parse(staff=StaffAtom, staff=StaffList) :-
   delay((
       atomic_list_concat(Atoms, ' ', StaffAtom),
       maplist(atom_number, Atoms, StaffList)
   )).
-mei_transform(dur=Atom, dur=N) :-
+parse(dur=Atom, dur=N) :-
   delay(atom_number(Atom, N)).
-mei_transform(oct=Atom, oct=N) :-
+parse(oct=Atom, oct=N) :-
   delay(atom_number(Atom, N)).
-mei_transform(len=Atom, len=N) :-
+parse(len=Atom, len=N) :-
   delay(vu(Atom, N)).
-
-mei_transform(element(measure, AttrIn, ChildsIn), element(measure, AttrOut, ChildsOut)) :-
-  mapsubterms(mei_transform, ChildsIn, ChildsOut),
+parse(element(measure, AttrIn, ChildsIn), element(measure, AttrOut, ChildsOut)) :-
+  mapsubterms(parse, ChildsIn, ChildsOut),
   selectchk(n=Atom, AttrIn, n=N, AttrOut),
   delay(atom_number(Atom, N)).
-
-mei_transform(element(staff, AttrIn, ChildsIn), element(staff, AttrOut, ChildsOut)) :-
-  mapsubterms(mei_transform, ChildsIn, ChildsOut),
+parse(element(staff, AttrIn, ChildsIn), element(staff, AttrOut, ChildsOut)) :-
+  mapsubterms(parse, ChildsIn, ChildsOut),
   selectchk(n=Atom, AttrIn, n=N, AttrOut),
   delay(atom_number(Atom, N)).
-
-mei_transform(element(staffDef, AttrIn, ChildsIn), element(staffDef, AttrOut, ChildsOut)) :-
-  mapsubterms(mei_transform, ChildsIn, ChildsOut),
+parse(element(staffDef, AttrIn, ChildsIn), element(staffDef, AttrOut, ChildsOut)) :-
+  mapsubterms(parse, ChildsIn, ChildsOut),
   selectchk(n=Atom, AttrIn, n=N, AttrOut),
   delay(atom_number(Atom, N)).
-
-mei_transform(element(meterSig, AttrIn, ChildsIn), element(meterSig, AttrOut, ChildsOut)) :-
-  mapsubterms(mei_transform, ChildsIn, ChildsOut),
+parse(element(meterSig, AttrIn, ChildsIn), element(meterSig, AttrOut, ChildsOut)) :-
+  mapsubterms(parse, ChildsIn, ChildsOut),
   selectchk(unit=Atom, AttrIn, unit=N, AttrOut),
   delay(atom_number(Atom, N)).
-
-mei_transform(Term, Term).
